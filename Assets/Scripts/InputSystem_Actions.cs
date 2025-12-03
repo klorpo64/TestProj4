0c81b8dbafdd29950b8c6ac58f81496f6e70ee2a
klorpo64 <klorpo64@gmail.com>
2025-12-03 18:10:26 -0500

    movement basics completed

--- Assets/Scripts/InputSystem_Actions.cs
+++ Assets/Scripts/InputSystem_Actions.cs
@@ -111,15 +111,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""initialStateCheck"": true
                 },
                 {
-                    ""name"": ""Attack"",
-                    ""type"": ""Button"",
-                    ""id"": ""6c2ab1b8-8984-453a-af3d-a3c78ae1679a"",
-                    ""expectedControlType"": ""Button"",
-                    ""processors"": """",
-                    ""interactions"": """",
-                    ""initialStateCheck"": false
-                },
-                {
                     ""name"": ""Interact"",
                     ""type"": ""Button"",
                     ""id"": ""852140f2-7766-474d-8707-702459ba45f3"",
@@ -177,6 +168,15 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""name"": ""Dance"",
                     ""type"": ""Button"",
                     ""id"": ""b3da63b2-c792-4e53-adc3-c08e0e52fe00"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Dive"",
+                    ""type"": ""Button"",
+                    ""id"": ""af8fa223-c29e-49a7-9824-f6a719914b33"",
                     ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
@@ -335,72 +335,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": ""Joystick"",
                     ""action"": ""Look"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""143bb1cd-cc10-4eca-a2f0-a3664166fe91"",
-                    ""path"": ""<Gamepad>/buttonWest"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Gamepad"",
-                    ""action"": ""Attack"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""05f6913d-c316-48b2-a6bb-e225f14c7960"",
-                    ""path"": ""<Mouse>/leftButton"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Keyboard&Mouse"",
-                    ""action"": ""Attack"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""886e731e-7071-4ae4-95c0-e61739dad6fd"",
-                    ""path"": ""<Touchscreen>/primaryTouch/tap"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": "";Touch"",
-                    ""action"": ""Attack"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""ee3d0cd2-254e-47a7-a8cb-bc94d9658c54"",
-                    ""path"": ""<Joystick>/trigger"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Joystick"",
-                    ""action"": ""Attack"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""8255d333-5683-4943-a58a-ccb207ff1dce"",
-                    ""path"": ""<XRController>/{PrimaryAction}"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""XR"",
-                    ""action"": ""Attack"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""b3c1c7f0-bd20-4ee7-a0f1-899b24bca6d7"",
-                    ""path"": ""<Keyboard>/enter"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Keyboard&Mouse"",
-                    ""action"": ""Attack"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 },
@@ -577,6 +511,17 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": "";Gamepad"",
                     ""action"": ""Dance"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""23fbd3fb-f3da-45bd-baba-0c72a735392e"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": "";Gamepad"",
+                    ""action"": ""Dive"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -1166,7 +1111,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
-        m_Player_Attack = m_Player.FindAction("Attack", throwIfNotFound: true);
         m_Player_Interact = m_Player.FindAction("Interact", throwIfNotFound: true);
         m_Player_Crouch = m_Player.FindAction("Crouch", throwIfNotFound: true);
         m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
@@ -1174,6 +1118,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         m_Player_Next = m_Player.FindAction("Next", throwIfNotFound: true);
         m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
         m_Player_Dance = m_Player.FindAction("Dance", throwIfNotFound: true);
+        m_Player_Dive = m_Player.FindAction("Dive", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1269,7 +1214,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Look;
-    private readonly InputAction m_Player_Attack;
     private readonly InputAction m_Player_Interact;
     private readonly InputAction m_Player_Crouch;
     private readonly InputAction m_Player_Jump;
@@ -1277,6 +1221,7 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Next;
     private readonly InputAction m_Player_Sprint;
     private readonly InputAction m_Player_Dance;
+    private readonly InputAction m_Player_Dive;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -1296,10 +1241,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Look".
         /// </summary>
         public InputAction @Look => m_Wrapper.m_Player_Look;
-        /// <summary>
-        /// Provides access to the underlying input action "Player/Attack".
-        /// </summary>
-        public InputAction @Attack => m_Wrapper.m_Player_Attack;
         /// <summary>
         /// Provides access to the underlying input action "Player/Interact".
         /// </summary>
@@ -1328,6 +1269,10 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Dance".
         /// </summary>
         public InputAction @Dance => m_Wrapper.m_Player_Dance;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Dive".
+        /// </summary>
+        public InputAction @Dive => m_Wrapper.m_Player_Dive;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -1360,9 +1305,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             @Look.started += instance.OnLook;
             @Look.performed += instance.OnLook;
             @Look.canceled += instance.OnLook;
-            @Attack.started += instance.OnAttack;
-            @Attack.performed += instance.OnAttack;
-            @Attack.canceled += instance.OnAttack;
             @Interact.started += instance.OnInteract;
             @Interact.performed += instance.OnInteract;
             @Interact.canceled += instance.OnInteract;
@@ -1384,6 +1326,9 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             @Dance.started += instance.OnDance;
             @Dance.performed += instance.OnDance;
             @Dance.canceled += instance.OnDance;
+            @Dive.started += instance.OnDive;
+            @Dive.performed += instance.OnDive;
+            @Dive.canceled += instance.OnDive;
         }
 
         /// <summary>
@@ -1401,9 +1346,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             @Look.started -= instance.OnLook;
             @Look.performed -= instance.OnLook;
             @Look.canceled -= instance.OnLook;
-            @Attack.started -= instance.OnAttack;
-            @Attack.performed -= instance.OnAttack;
-            @Attack.canceled -= instance.OnAttack;
             @Interact.started -= instance.OnInteract;
             @Interact.performed -= instance.OnInteract;
             @Interact.canceled -= instance.OnInteract;
@@ -1425,6 +1367,9 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
             @Dance.started -= instance.OnDance;
             @Dance.performed -= instance.OnDance;
             @Dance.canceled -= instance.OnDance;
+            @Dive.started -= instance.OnDive;
+            @Dive.performed -= instance.OnDive;
+            @Dive.canceled -= instance.OnDive;
         }
 
         /// <summary>
@@ -1740,13 +1685,6 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnLook(InputAction.CallbackContext context);
         /// <summary>
-        /// Method invoked when associated input action "Attack" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
-        /// </summary>
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
-        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
-        void OnAttack(InputAction.CallbackContext context);
-        /// <summary>
         /// Method invoked when associated input action "Interact" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
         /// </summary>
         /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
@@ -1795,6 +1733,13 @@ public partial class @PlayerControls: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnDance(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Dive" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnDive(InputAction.CallbackContext context);
     }
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "UI" which allows adding and removing callbacks.
--- Assets/Scripts/PlatformerController.cs
+++ Assets/Scripts/PlatformerController.cs
@@ -4,54 +4,96 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class PlatformerController : MonoBehaviour
 {
-    [Header("Movement Settings")]
+    [Header("Movement")]
     public float moveSpeed = 6f;
     public float crouchSpeed = 3f;
     public float crawlSpeed = 1.2f;
-    public float gravity = -25f;
+
+    [Header("Jump Settings")]
     public float jumpHeight = 2.5f;
-    public float doubleJumpHeight = 1.25f;
+    public float doubleJumpHeight = 1.2f;
     public float crouchJumpHeight = 3.5f;
+    public float gravity = -25f;
 
-    [Header("Capsule Heights")]
-    public float standingHeight = 2f;
-    public float crouchingHeight = 1.2f;
-    public float crawlingHeight = 0.8f;
-    public float heightSmooth = 8f;
+    [Header("Dive")]
+    public float diveSpeed = 14f;
+    public float diveDuration = 0.35f;
+    public float diveForwardBoost = 1.5f;
 
-    [Header("Fall / Landing")]
-    public float fallThreshold = -10f;
+    [Header("Dive Bounce")]
+    public float diveInitialBounce = 1.2f;
+
+    [Header("Dive Landing Roll")]
+    public float rollSpeed = 10f;
+    public float rollDuration = 0.6f;
+    public float rollDecay = 5f;
+
+    [Header("Crouch Charge")]
+    public float requiredCrouchHold = 3f;
+
+    [Header("Hard Landing")]
+    public float hardLandingVelocity = -30f;
+    public float stunDuration = 1f;
+
+    [Header("Collider Settings")]
+    public float crouchHeightMultiplier = 0.5f;
+
+    [Header("Ceiling Check")]
+    public LayerMask ceilingLayers;
 
     [Header("References")]
-    public Transform cameraTransform;
     public Animator animator;
+    public Transform cameraTransform;
 
-    private PlayerControls controls;
     private CharacterController controller;
+    private PlayerControls controls;
 
     private Vector2 moveInput;
-    private bool crouchHeld;
+    private Vector3 velocity;
+    private Vector3 horizVelocity;
 
+    private bool crouchHeld;
     private bool isCrouching;
     private bool isCrawling;
+    private float crouchHoldTime = 0f;
 
-    private Vector3 velocity;               // vertical velocity
-    private Vector3 horizontalVelocity;     // our own tracked horizontal velocity
     private bool canDoubleJump;
     private bool jumpStarted;
+
+    // Dive
+    private bool isDiving;
+    private float diveTimer;
+    private Vector3 diveDirection;
+    private bool hasDivedThisAir = false;
+
+    // Roll
+    private bool isRolling;
+    private float rollTimer;
+    private Vector3 rollVelocity;
+
+    // Hard landing stun
+    private bool isStunned = false;
+    private float stunTimer = 0f;
+
+    // Collider original values
+    private float originalHeight;
+    private Vector3 originalCenter;
 
     void Awake()
     {
         controller = GetComponent<CharacterController>();
         controls = new PlayerControls();
 
+        originalHeight = controller.height;
+        originalCenter = controller.center;
+
         controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
 
         controls.Player.Jump.performed += ctx => StartJump();
-
         controls.Player.Crouch.performed += ctx => crouchHeld = true;
         controls.Player.Crouch.canceled += ctx => crouchHeld = false;
+        controls.Player.Dive.performed += ctx => StartDive();
     }
 
     void OnEnable() => controls.Enable();
@@ -59,27 +101,78 @@ public class PlatformerController : MonoBehaviour
 
     void Update()
     {
+        // ------------------------
+        // Handle stun first
+        // ------------------------
+        if (isStunned)
+        {
+            stunTimer -= Time.deltaTime;
+            horizVelocity = Vector3.zero;
+            velocity.y = -2f;
+
+            if (stunTimer <= 0f)
+                isStunned = false;
+
+            controller.Move(new Vector3(0, velocity.y, 0) * Time.deltaTime);
+            UpdateAnimator();
+            return;
+        }
+
+        // Reset dive ability when grounded
+        if (controller.isGrounded)
+            hasDivedThisAir = false;
+
         UpdateCrouchState();
-        HandleMovement();
+
+        if (isDiving)
+            HandleDive();
+        else if (isRolling)
+            HandleRoll();
+        else
+            HandleMovement();
+
         ApplyGravity();
 
-        // Apply full movement
-        Vector3 finalMove = horizontalVelocity + new Vector3(0, velocity.y, 0);
+        Vector3 finalMove = horizVelocity + new Vector3(0, velocity.y, 0);
         controller.Move(finalMove * Time.deltaTime);
+
+        // ------------------------
+        // Hard landing detection
+        // ------------------------
+        if (controller.isGrounded && velocity.y <= hardLandingVelocity && !isStunned)
+        {
+            isStunned = true;
+            stunTimer = stunDuration;
+
+            horizVelocity = Vector3.zero;
+            isDiving = false;
+            isRolling = false;
+        }
 
         UpdateAnimator();
     }
 
-    // ---------------------
-    // CROUCH / CRAWL LOGIC (hold button, grounded only)
-    // ---------------------
+    // ------------------------
+    // CROUCH / CRAWL / COLLIDER / CEILING CHECK
+    // ------------------------
     void UpdateCrouchState()
     {
-        bool isMoving = moveInput.sqrMagnitude > 0.01f;
-
-        if (controller.isGrounded && crouchHeld) // Only crouch/crawl if grounded AND button held
+        if (isStunned)
         {
-            if (isMoving)
+            ResetCollider();
+            return;
+        }
+
+        bool grounded = controller.isGrounded;
+        bool moving = moveInput.sqrMagnitude > 0.01f;
+
+        bool forcedCrouch = !CanStand(); // Force crouch if ceiling above
+
+        if (grounded && (crouchHeld || forcedCrouch))
+        {
+            crouchHoldTime += Time.deltaTime;
+
+            if (moving)
             {
                 isCrawling = true;
                 isCrouching = false;
@@ -89,28 +182,53 @@ public class PlatformerController : MonoBehaviour
                 isCrouching = true;
                 isCrawling = false;
             }
+
+            controller.height = originalHeight * crouchHeightMultiplier;
+            controller.center = new Vector3(originalCenter.x, originalCenter.y * crouchHeightMultiplier, originalCenter.z);
         }
-        else // Not grounded or button released > stand
+        else
         {
-            isCrouching = false;
-            isCrawling = false;
+            crouchHoldTime = 0f;
+
+            if (!forcedCrouch)
+            {
+                // Enough space to stand
+                isCrouching = false;
+                isCrawling = false;
+                controller.height = originalHeight;
+                controller.center = originalCenter;
+            }
+            else
+            {
+                // Ceiling too low, force crouch
+                isCrouching = true;
+                isCrawling = true;
+                controller.height = originalHeight * crouchHeightMultiplier;
+                controller.center = new Vector3(originalCenter.x, originalCenter.y * crouchHeightMultiplier, originalCenter.z);
+            }
         }
-
-        // Update capsule height
-        float targetHeight = standingHeight;
-        if (isCrawling) targetHeight = crawlingHeight;
-        else if (isCrouching) targetHeight = crouchingHeight;
-
-        controller.height = Mathf.Lerp(controller.height, targetHeight, Time.deltaTime * heightSmooth);
-
-        Vector3 c = controller.center;
-        c.y = controller.height / 2f;
-        controller.center = c;
     }
 
-    // ---------------------
+    bool CanStand()
+    {
+        Vector3 bottom = transform.position + controller.center - Vector3.up * (controller.height / 2 - controller.radius);
+        Vector3 top = bottom + Vector3.up * originalHeight;
+
+        return !Physics.CheckCapsule(bottom, top, controller.radius, ceilingLayers);
+    }
+
+    void ResetCollider()
+    {
+        crouchHoldTime = 0f;
+        isCrouching = false;
+        isCrawling = false;
+        controller.height = originalHeight;
+        controller.center = originalCenter;
+    }
+
+    // ------------------------
     // MOVEMENT
-    // ---------------------
+    // ------------------------
     void HandleMovement()
     {
         float speed =
@@ -123,88 +241,144 @@ public class PlatformerController : MonoBehaviour
 
         Vector3 moveDir = forward * moveInput.y + right * moveInput.x;
 
-        horizontalVelocity = moveDir * speed;
+        horizVelocity = moveDir * speed;
 
-        // Rotate the model toward movement
         if (moveDir.sqrMagnitude > 0.01f)
-        {
-            Quaternion targetRot = Quaternion.LookRotation(moveDir);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, 10f * Time.deltaTime);
-        }
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDir), 10f * Time.deltaTime);
         else
-        {
-            horizontalVelocity = Vector3.zero;
-        }
+            horizVelocity = Vector3.zero;
     }
 
-    // ---------------------
-    // JUMP / GRAVITY
-    // ---------------------
+    // ------------------------
+    // JUMP
+    // ------------------------
     void StartJump()
     {
+        if (isStunned || isDiving || isRolling)
+            return;
+
         if (isCrouching || isCrawling)
         {
-            velocity.y = Mathf.Sqrt(crouchJumpHeight * -2f * gravity);
-            jumpStarted = true;
+            if (crouchHoldTime >= requiredCrouchHold)
+                Jump(crouchJumpHeight);
+            else
+                Jump(jumpHeight);
             return;
         }
 
         if (controller.isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            Jump(jumpHeight);
             canDoubleJump = true;
-            jumpStarted = true;
         }
         else if (canDoubleJump)
         {
-            velocity.y = Mathf.Sqrt(doubleJumpHeight * -2f * gravity);
+            Jump(doubleJumpHeight);
             canDoubleJump = false;
-            jumpStarted = true;
         }
     }
 
+    void Jump(float height)
+    {
+        velocity.y = Mathf.Sqrt(height * -2f * gravity);
+        jumpStarted = true;
+    }
+
+    // ------------------------
+    // GRAVITY
+    // ------------------------
     void ApplyGravity()
     {
         if (!controller.isGrounded)
-        {
             velocity.y += gravity * Time.deltaTime;
-        }
         else if (velocity.y < 0)
+            velocity.y = -2f;
+    }
+
+    // ------------------------
+    // DIVE
+    // ------------------------
+    void StartDive()
+    {
+        if (isStunned || controller.isGrounded || isDiving || isRolling || hasDivedThisAir)
+            return;
+
+        hasDivedThisAir = true;
+        isDiving = true;
+        diveTimer = diveDuration;
+
+        velocity.y = Mathf.Sqrt(diveInitialBounce * -2f * gravity);
+
+        diveDirection = (transform.forward * diveForwardBoost + Vector3.down * 0.25f).normalized;
+        horizVelocity = new Vector3(diveDirection.x, 0f, diveDirection.z) * diveSpeed;
+        velocity.y += diveDirection.y * diveSpeed;
+    }
+
+    void HandleDive()
+    {
+        diveTimer -= Time.deltaTime;
+
+        horizVelocity = new Vector3(diveDirection.x, 0f, diveDirection.z) * diveSpeed;
+        velocity.y += gravity * Time.deltaTime * 0.6f;
+
+        if (controller.isGrounded)
         {
-            velocity.y = -2f; // keep grounded
+            isDiving = false;
+            StartRoll();
+            return;
+        }
+
+        if (diveTimer <= 0f)
+            isDiving = false;
+    }
+
+    // ------------------------
+    // ROLL
+    // ------------------------
+    void StartRoll()
+    {
+        isRolling = true;
+        rollTimer = rollDuration;
+
+        Vector3 rollDir = new Vector3(diveDirection.x, 0, diveDirection.z).normalized;
+        rollVelocity = rollDir * rollSpeed;
+        transform.rotation = Quaternion.LookRotation(rollDir);
+    }
+
+    void HandleRoll()
+    {
+        rollTimer -= Time.deltaTime;
+
+        horizVelocity = rollVelocity;
+        rollVelocity = Vector3.Lerp(rollVelocity, Vector3.zero, rollDecay * Time.deltaTime);
+
+        if (rollTimer <= 0f)
+        {
+            isRolling = false;
+            rollVelocity = Vector3.zero;
         }
     }
 
-    // ---------------------
+    // ------------------------
     // ANIMATOR
-    // ---------------------
+    // ------------------------
     void UpdateAnimator()
     {
-        animator.SetFloat("Speed", horizontalVelocity.magnitude);
+        if (animator == null) return;
 
-        animator.SetBool("IsCrouching", isCrouching);
-        animator.SetBool("IsCrawling", isCrawling);
+        animator.SetFloat("Speed", horizVelocity.magnitude);
+        animator.SetFloat("VerticalVelocity", velocity.y);
 
         animator.SetBool("IsGrounded", controller.isGrounded);
-
-        animator.SetFloat("VerticalVelocity", velocity.y);
+        animator.SetBool("IsCrouching", isCrouching);
+        animator.SetBool("IsCrawling", isCrawling);
+        animator.SetBool("IsDiving", isDiving);
+        animator.SetBool("IsRolling", isRolling);
 
         if (jumpStarted)
         {
             animator.SetTrigger("JumpTriggered");
             jumpStarted = false;
-        }
-
-        if (controller.isGrounded && velocity.y < fallThreshold)
-        {
-            animator.SetTrigger("LandHard");
-        }
-
-        // Smooth transition back to standing (optional, animation already handles this)
-        if (!crouchHeld && !isCrawling && !isCrouching)
-        {
-            animator.SetBool("IsCrouching", false);
-            animator.SetBool("IsCrawling", false);
         }
     }
 }
